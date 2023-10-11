222f2c5f025b47de85471d842ac8c0ca0498f216
Gabriel Guedes <gabrielguedesbsilva@gmail.com>
2023-10-10 22:04:45 -0300

    Criando Template e Mapeando Urls da Página Create Contact

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,32 @@
+from typing import Any
+from django.shortcuts import render, get_object_or_404, redirect
+from contact.models import Contact
+from django.db.models import Q
+
+from contact.forms import ContactForm
+
+
+def create (request):
+    # Se o método for POST, ou seja, o usuário enviar o formulário no nosso caso ele vai salvar as informações do form
+    if request.method == 'POST':
+        data = request.POST
+    
+        context = {
+            'form': ContactForm(data)
+        }
+        
+        return render (
+            request,
+            'contact/create.html',
+            context
+        )
+    
+    context = {
+            'form': ContactForm()
+        }
+    
+    return render (
+            request,
+            'contact/create.html',
+            context
+        )
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -6,7 +6,7 @@ from django.core.paginator import Paginator
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
     
-    paginator = Paginator(contacts, 20)
+    paginator = Paginator(contacts, 10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     
@@ -53,7 +53,7 @@ def search (request):
         Q(email__icontains=search_value)
         ).order_by('-id')
 
-    paginator = Paginator(contacts, 5)
+    paginator = Paginator(contacts, 10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
