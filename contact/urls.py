afe14761da7ebde7f96b6b405e8b85d4775202ed
Gabriel Guedes <gabrielguedesbsilva@gmail.com>
2023-10-19 17:48:36 -0300

    Criação da funcionalidade de deleção de contatos

--- contact/urls.py
+++ contact/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('contact/detail/<int:contact_id>/', views.contact, name='contact'),
     path('contact/create/', views.create, name='create'),
     path('contact/update/<int:contact_id>/', views.update, name='update'),
+    path('contact/delete/<int:contact_id>/', views.delete, name='delete'),
 ]
